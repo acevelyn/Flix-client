1c4394b93715da9ef1c308cbb574cdf46f272409
acevelyn <86307435+acevelyn@users.noreply.github.com>
2021-10-04 13:00:57 -0400

    Changes

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 export class MovieView extends React.Component {
 
@@ -34,3 +35,19 @@ export class MovieView extends React.Component {
     );
   }
 }
+
+MovieCard.propTypees = {
+  movie: PropTypes.shape({
+    Title: PropTypes.string.isRequired,
+    Description: PropTypes.string.isRequired,
+    ImagePath: PropTypes.string.isRequired,
+    Genre: PropTypes.shape({
+      Name: PropTypes.string,
+      Description: PropTypes.string,
+      Director: PropTypes.shape({
+        Name: PropTypes.string.isRequired,
+        Bio: PropTypes.string
+      })
+
+    })
+  }).isRequired,
