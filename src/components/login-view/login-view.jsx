3c02b5aea55c62ee9b95eed0274382e8318074c9
acevelyn <86307435+acevelyn@users.noreply.github.com>
2021-10-07 16:44:18 -0400

    Update login-view.jsx

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import axios from 'axios';
 import PropTypes from 'prop-types';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
@@ -11,10 +12,18 @@ export function LoginView(props) {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(username, password);
     /* Send a request to the server for authentication */
-    /* then call props.onLoggedIn(username) */
-    props.onLoggedIn(username);
+    axios.post('https://evflixapp.herokuapp.com/login', {
+      Username: username,
+      Password: password
+    }) /* If data matches what is in database, then call props.onLoggedIn(username) */
+      .then(response => {
+        const data = response.data;
+        props.onLoggedIn(data);
+      })
+      .catch(e => {
+        console.log('No such user')
+      });
   };
 
   return (
@@ -22,12 +31,12 @@ export function LoginView(props) {
       <h1 className="login-title">Login</h1>
       <Form.Group controlId="formUsername">
         <Form.Label>Username:</Form.Label>
-        <Form.Control type="text" placeholder="Enter username" onChange={e => setUsername(e.target.value)} />
+        <Form.Control type="text" placeholder="Enter username" value={username} onChange={e => setUsername(e.target.value)} />
       </Form.Group>
 
       <Form.Group controlId="formPassword">
         <Form.Label>Password:</Form.Label>
-        <Form.Control type="text" placeholder="Enter password" onChange={e => setUsername(e.target.value)} />
+        <Form.Control type="text" placeholder="Enter password" value={password} onChange={e => setUsername(e.target.value)} />
       </Form.Group>
 
       <Button variant="primary" size="md" type="submit" onClick={handleSubmit}>
