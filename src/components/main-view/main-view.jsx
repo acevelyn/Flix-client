6a9aa399fdf93027035222b2f6f6dc94a18df68d
acevelyn <86307435+acevelyn@users.noreply.github.com>
2021-10-10 10:53:51 -0400

    Updates

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -11,7 +11,7 @@ import { MovieCard } from '../movie-card/movie-card';
 // React Bootstrap Styling
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import Navbar from 'react-bootstrap/Navbar';
+
 
 
 import './main-view.scss';
@@ -22,12 +22,11 @@ export class MainView extends React.Component {
     super();
     this.state = {
       movies: [],
-      selectedMovie: null,
+      // selectedMovie: null,
       user: null,
-      register: null
+      // register: null
     };
   }
-
 
   componentDidMount() {
     let accessToken = localStorage.getItem('token');
@@ -39,25 +38,27 @@ export class MainView extends React.Component {
     }
   }
 
-  /*When a movie is clicked, this function is invoked and updates 
-    the state of the `selectedMovie` *property to that movie*/
-
-  setSelectedMovie(movie) {
-    this.setState({
-      selectedMovie: movie
-    });
-  }
-
   // Log In
   onLoggedIn(authData) {
     console.log(authData);
     this.setState({
       user: authData.user.Username
     });
-    localStorage.setItem('token', authData);
+
+    localStorage.setItem('token', authData.token);
     localStorage.setItem('user', authData.user.Username);
     this.getMovies(authData.token);
   }
+
+
+  /*When a movie is clicked, this function is invoked and updates 
+    the state of the `selectedMovie` *property to that movie*/
+
+  // setSelectedMovie(movie) {
+  //   this.setState({
+  //     selectedMovie: movie
+  //   });
+  // }
 
   // Get All Movies
   getMovies(token) {
@@ -83,7 +84,7 @@ export class MainView extends React.Component {
   }
 
   render() {
-    const { movies, selectedMovie, user, register } = this.state;
+    const { movies, user } = this.state;
 
     if (!register) return <RegistrationView onRegistration={(register) => this.onRegistration(register)} />;
 
