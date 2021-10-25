40b728943b55440f2380b312197fea2f77e16e73
acevelyn <86307435+acevelyn@users.noreply.github.com>
2021-10-25 12:40:35 -0400

    Update profile-view.jsx

--- src/components/profile-view/profile-view.jsx
+++ src/components/profile-view/profile-view.jsx
@@ -4,7 +4,7 @@
 import React from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
-import UserInfo  from './user-info';
+import UserInfo from './user-info';
 import FavMovieView from './favorite-movies';
 import UpdateUser from './update-user';
 
@@ -14,7 +14,7 @@ import { Row, Container, Col, Button, Form, Card } from 'react-bootstrap';
 
 
 export class ProfileView extends React.Component {
-  constructor(){
+  constructor() {
     super();
     this.state = {
       Username: null,
@@ -29,9 +29,9 @@ export class ProfileView extends React.Component {
     };
   }
 
-  componentDidMount(){
+  componentDidMount() {
     console.log('PROFILE VIEW');
-    const accessToken = localStorage.getItem('token'); 
+    const accessToken = localStorage.getItem('token');
     if (accessToken !== null) {
       this.getUser(accessToken);
     }
@@ -39,118 +39,121 @@ export class ProfileView extends React.Component {
 
   // ALL FUNCTIONS WITH HTTP REQUESTS TO GET, UPDATE, DELETE 
 
-   // GET User Info
-  getUser(token) { 
+  // GET User Info
+  getUser(token) {
     const username = localStorage.getItem('user');
     axios.get(
-      `https://evflixapp.herokuapp.com/users/${username}`, 
-      { headers: { Authorization: `Bearer ${token}` },
-    })
-    .then((response)=> {
-      console.log(response);
-      this.setState({
-        Username: response.data.Username,
-        Email: response.data.Email,
-        Birthday: response.data.Birthday,
-        FavoriteMovies: response.data.FavoriteMovies
-      });
-    })
-    .catch((error)=> {
-      console.log(error);
-    })
+      `https://evflixapp.herokuapp.com/users/${username}`,
+      {
+        headers: { Authorization: `Bearer ${token}` },
+      })
+      .then((response) => {
+        console.log(response);
+        this.setState({
+          Username: response.data.Username,
+          Email: response.data.Email,
+          Birthday: response.data.Birthday,
+          FavoriteMovies: response.data.FavoriteMovies
+        });
+      })
+      .catch((error) => {
+        console.log(error);
+      })
   }
 
-   // Update User Info / PUT Request 1
-   handleUpdate(newUsername, newPassword, newEmail) {
+  // Update User Info / PUT Request 1
+  handleUpdate(newUsername, newPassword, newEmail) {
     const token = localStorage.getItem('token');
     const username = localStorage.getItem('user');
 
     axios.put(
-      `https://evflixapp.herokuapp.com/users/${username}`, 
-        {
+      `https://evflixapp.herokuapp.com/users/${username}`,
+      {
         Username: newUsername,
         Password: newPassword,
         Email: newEmail,
         // Birthday: newBirthday
         // FavoriteMovies: []
       },
-      { headers: { Authorization: `Bearer ${token}`} }
+      { headers: { Authorization: `Bearer ${token}` } }
     )
-    .then((response)=> {
-      this.setState({
-        Username: response.data.Username,
-        Password: response.data.Password,
-        Email: response.data.Email,
-        // Birthday: response.data.Birthday
+      .then((response) => {
+        this.setState({
+          Username: response.data.Username,
+          Password: response.data.Password,
+          Email: response.data.Email,
+          // Birthday: response.data.Birthday
+        });
+        localStorage.setItem('user', response.data.Username)
+        alert('Account has been updated')
+        window.open(`/users/${username}`, '_self');
+      })
+      .catch((error) => {
+        console.log(error)
       });
-      localStorage.setItem('user',response.data.Username)
-      alert('Account has been updated')
-      window.open(`/users/${username}`, '_self');
-    })
-    .catch((error)=> {
-      console.log(error)
-    });
   }
-  setUsername(username){
+  setUsername(username) {
     this.setState({ tempUsername: username });
   }
 
-  setPassword(password){
+  setPassword(password) {
     this.setState({ tempPassword: password });
   }
 
-  setEmail(email){
+  setEmail(email) {
     this.setState({ tempEmail: email });
   }
 
 
-   // Deregister User
-  handleDeregister(user){
+  // Deregister User
+  handleDeregister(user) {
     const token = localStorage.getItem('token');
     const username = localStorage.getItem('user');
     axios.delete(
       `http://exflixapp.herokuapp.com/users/${username}`,
-     { headers: { Authorization: `Bearer ${token}`},
-     })
-    .then(()=> {
-      localStorage.removeItem('token');
-      localStorage.removeItem('user');
+      {
+        headers: { Authorization: `Bearer ${token}` },
+      })
+      .then(() => {
+        localStorage.removeItem('token');
+        localStorage.removeItem('user');
         this.setState({
-        user: null  // is this correct? user is not a state?
+          user: null  // is this correct? user is not a state?
         })
         alert('Account has been deleted')
-    })
-    .catch((err)=> {
-      console.log('your error is '+err);
-    });
+      })
+      .catch((err) => {
+        console.log('your error is ' + err);
+      });
   }
 
   // Remove A Favorite Movie
-  removeFavoriteMovie(movie) {
+  removeFavoriteMovie(_id) {
 
     const token = localStorage.getItem('token');
     const username = localStorage.getItem('user');
 
-    axios.delete(`https://evflixapp.herokuapp.com/users/${username}/movies/${movie._id}`,
-    { headers: { Authorization: `Bearer ${token}` },
-    })
-    .then(() =>{
-    console.log('Movie was removed')
-    })
-    .catch((error)=> {
-    console.log(error);
-    })
-    }
+    axios.delete(`https://evflixapp.herokuapp.com/users/${username}/movies/${_id}`,
+      {
+        headers: { Authorization: `Bearer ${token}` },
+      })
+      .then((response) => {
+        console.log('Movie was removed')
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+  }
 
-   // RENDER THIS..
-  render(){
-      return (
-        <Container>
+  // RENDER THIS..
+  render() {
+    return (
+      <Container>
         <Row>
           <Col xs={12} sm={4}>
             <Card>
               <Card.Body>
-                <UserInfo name={this.state.Username} email={this.state.Email} birthday={this.state.Birthday}/>
+                <UserInfo name={this.state.Username} email={this.state.Email} birthday={this.state.Birthday} />
               </Card.Body>
             </Card>
           </Col>
@@ -158,23 +161,24 @@ export class ProfileView extends React.Component {
           <Col xs={12} sm={8}>
             <Card>
               <Card.Body>
-                <UpdateUser tempEmail={this.state.tempEmail} tempPassword={this.state.tempPassword} tempUsername={this.state.tempUsername} user={this.state.user}
-              handleUpdate={this.handleUpdate}/>
+                <UpdateUser setUsername={this.setUsername} setPassword={this.setPassword} setEmail={this.setEmail} user={this.state.user}
+                  handleUpdate={this.handleUpdate} se />
               </Card.Body>
             </Card>
           </Col>
-       </Row>
-        <FavMovieView favoriteMovieList={this.state.FavoriteMovies} movies={this.props.movies} removeFav={this.removeFavoriteMovie}/>
-        
+        </Row>
+        <FavMovieView favoriteMovieList={this.state.FavoriteMovies} movies={this.props.movies} removeFav={this.removeFavoriteMovie} />
 
 
-    {/* <div className="logout-button">
+
+        {/* <div className="logout-button">
       <Button variant="link" onClick={()=> { this.onLoggedOut() }}>Logout</Button>
     </div> */}
 
-     </Container>
+      </Container>
     )
-  }}
+  }
+}
 
 
 
