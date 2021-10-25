73d4e36a23935f6cd6a4d2a722086a41d6c8959f
acevelyn <86307435+acevelyn@users.noreply.github.com>
2021-10-25 12:41:06 -0400

    Update update-user.jsx

--- src/components/profile-view/update-user.jsx
+++ src/components/profile-view/update-user.jsx
@@ -5,57 +5,53 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 // Stylings
-import {Form, Button } from 'react-bootstrap';
+import { Form, Button } from 'react-bootstrap';
 
 
 
-function UpdateUser({ tempUsername, tempEmail, tempPassword, user}) {
+function UpdateUser({ setUsername, setPassword, setEmail, user }) {
   return (
     <Form>
       <h2 className="update-title">Update Account Info</h2>
-        <Form.Group controlId="formUsername">
-            <Form.Label>Username:</Form.Label>
-            <Form.Control 
-            type="text"
-            // defaultValue={user.Username} HOW CAN I SET THE DEFAULT VALUE??
-            onChange={e => {setUsername(e.target.value)}} 
-            required
-            placeholder="Enter New Username"
-            />
-        </Form.Group>
+      <Form.Group controlId="formUsername">
+        <Form.Label>Username:</Form.Label>
+        <Form.Control
+          type="text"
+          // defaultValue={user.Username} HOW CAN I SET THE DEFAULT VALUE??
+          onChange={e => { setUsername(e.target.value) }}
+          required
+          placeholder="Enter New Username"
+        />
+      </Form.Group>
 
       <Form.Group controlId="formPassword">
         <Form.Label>Password:</Form.Label>
-        <Form.Control 
-        type="password" 
-        defaultValue=''
-        onChange={e => {setPassword(e.target.value)}}
-        required
-        minLength="8"
-        placeholder="New Password Must be at least 8 Characters" />
+        <Form.Control
+          type="password"
+          defaultValue=''
+          onChange={e => { setPassword(e.target.value) }}
+          required
+          minLength="8"
+          placeholder="New Password Must be at least 8 Characters" />
       </Form.Group>
 
       <Form.Group controlId="formEmail">
         <Form.Label>Email:</Form.Label>
-        <Form.Control 
+        <Form.Control
           type="text"
           onChange={e => setEmail(e.target.value)}
           placeholder="Enter New Email"
-          />
+        />
       </Form.Group>
-      
+
       {/* Submit Button */}
-      <Button 
-      variant="primary" 
-      size="md" 
-      type="submit" 
-      onClick={(e) => {
-        e.preventDefault();
-        handleUpdate(
-          tempUsername,
-          tempPassword,
-          tempEmail
-          );
+      <Button
+        variant="primary"
+        size="md"
+        type="submit"
+        onClick={(e) => {
+          e.preventDefault();
+          handleUpdate(e);
         }
         }>
         Submit
@@ -66,13 +62,13 @@ function UpdateUser({ tempUsername, tempEmail, tempPassword, user}) {
         <br />
         <h4>Delete Account</h4>
 
-        <Button variant="danger" size="sm" type="submit" 
-          onClick={(e)=> {
+        <Button variant="danger" size="sm" type="submit"
+          onClick={(e) => {
             e.preventDefault();
             handleDeregister(user);
-            }}>Deregister
+          }}>Deregister
         </Button>
-        <br/>
+        <br />
       </div>
 
     </Form>
