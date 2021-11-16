41c027600575dad7053a7979baf5458100b696eb
acevelyn <86307435+acevelyn@users.noreply.github.com>
2021-11-16 16:14:16 -0500

    Update actions.js

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -2,33 +2,29 @@
 export const SET_MOVIES = 'SET_MOVIES';
 export const SET_FILTER = 'SET_FILTER';
 export const SET_USER = 'SET_USER';
+export const UPDATE_USER = 'UPDATE_USER';
 
 // GENERAL MOVIES
 export function setMovies(value) {
   console.log('SET_MOVIES action reached');
-  return {
-    type: SET_MOVIES,
-    value
-  };
+  return { type: SET_MOVIES, value };
 }
 
 export function setFilter(value) {
   console.log('SET_FILTER action reached');
-  return {
-    type: SET_FILTER,
-    value
-  };
+  return { type: SET_FILTER, value };
 }
 
 // USER ACTIONS
 export function setUser(value) {
   console.log('SET_USER action reached');
-  return {
-    type: SET_USER,
-    value
-  };
+  return { type: SET_USER, value };
 }
 
+export function updateUser(value) {
+  console.log('UPDATE_USER action reached');
+  return { type: UPDATE_USER, value };
+}
 
 
 
