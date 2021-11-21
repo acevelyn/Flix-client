7a6e8dc024b229b48a66415d40191aed3537e187
acevelyn <86307435+acevelyn@users.noreply.github.com>
2021-11-21 09:45:04 -0500

    Update favorite-movies.jsx

--- src/components/profile-view/favorite-movies.jsx
+++ src/components/profile-view/favorite-movies.jsx
@@ -4,7 +4,11 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
-import axios from 'axios'
+import { connect } from 'react-redux';
+import { updateUser } from '../../actions/actions';
+
+
+
 
 // Stylings
 import './profile-view.scss';
@@ -60,4 +64,4 @@ FavMovieView.propTypes = {
   removeFavoriteMovie: PropTypes.func,
 }
 
-export default FavMovieView
+export default connect(null, { updateUser })(FavMovieView)
